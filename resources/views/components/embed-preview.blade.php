42da5cef05e76d41ba3744d7e225827a71e702d0
LeaOhyeah <leasehat@gmail.com>
2025-02-05 11:28:55 +0800

    [delete] menghapus view lama dan file tidak digunakan

--- resources/views/components/embed-preview.blade.php
+++
@@ -1,3 +0,0 @@
-<div x-data="{ url: @entangle($attributes->wire('url')) }">
-    <iframe x-if="url" width="560" height="315" src="https://www.youtube.com/embed/{{ $url }}" frameborder="0" allowfullscreen></iframe>
-</div>
